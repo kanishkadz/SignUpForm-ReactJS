48cbacf9b08e8f7388278a81e4ebe3f350353ea7
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-01 18:59:29 +0530

    Update LoginSignup.jsx

--- signup/src/Components/LoginSignup/LoginSignup.jsx
+++ signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -12,7 +12,7 @@ function LoginSignup() {
   return (
     <div className='container'>
         <div className='header'>
-            <div className='text'>Sign Up</div>
+            <div className='text'>{action}</div>
             <div className='underline'> </div>
         </div>
         <div className='inputs'>
@@ -31,8 +31,8 @@ function LoginSignup() {
         </div>
         <div className='forgot-password'>Lost Password? <span>Click Here!</span></div>
         <div className='submit-container'>
-            <div className='submit'>Sign Up</div>
-            <div className='submit'>Login</div>
+            <div className={action=="Login" ? "submit gray" : "submit"}>Sign Up</div>
+            <div className={action=="Sign Up" ? "submit gray" : "submit"}>Login</div>
         </div>        
     </div>
   )
