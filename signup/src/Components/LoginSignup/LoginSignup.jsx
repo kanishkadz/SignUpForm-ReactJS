44a274413eccb4c6563f64d4956d7b072ac58b45
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-01 18:35:27 +0530

    Update LoginSignup.jsx

--- signup/src/Components/LoginSignup/LoginSignup.jsx
+++ signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -26,7 +26,10 @@ function LoginSignup() {
                 <input type="password" />
             </div>
         </div>
-        
+        <div className='submit-container'>
+            <div className='submit'>Sign Up</div>
+            <div className='submit'>Log In</div>
+        </div>        
     </div>
   )
 }
