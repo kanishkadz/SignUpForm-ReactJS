d0fa5ecb78522075c76b55faf07cbd99234322c3
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-01 18:29:24 +0530

    Update LoginSignup.jsx

--- signup/src/Components/LoginSignup/LoginSignup.jsx
+++ signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './LoginSignup.css'
 
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
 function LoginSignup() {
   return (
     <div>LoginSignup</div>
